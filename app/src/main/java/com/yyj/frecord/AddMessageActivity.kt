b4b3d3a665fc76254ae8335040f23ec937503d17
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-11-12 23:48:37 +0900

    calendar & timepicker

--- app/src/main/java/com/yyj/frecord/AddMessageActivity.kt
+++ app/src/main/java/com/yyj/frecord/AddMessageActivity.kt
@@ -1,48 +1,77 @@
 package com.yyj.frecord
 
-import android.app.AlarmManager
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.app.*
 import android.content.Context
-import android.content.Intent
+import android.content.DialogInterface
 import android.os.Build
 import android.os.Bundle
-import android.os.SystemClock
+import android.util.Log
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_message.*
+import kotlinx.android.synthetic.main.dialog_datepicker.view.*
 import java.util.*
 
 
 class AddMessageActivity : AppCompatActivity() {
     private val CHANNEL_ID = "msg"
-    private lateinit var intent: PendingIntent
+    private lateinit var dialog : AlertDialog
+    private lateinit var intent : PendingIntent
+    private val alarmCalendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_message)
 
+        setAlarmDate()
+        alarmCalendar.set(Calendar.HOUR, timePicker.hour)
+        alarmCalendar.set(Calendar.MINUTE, timePicker.minute)
+        alarmCalendar.set(Calendar.SECOND, 0)
         createNotificationChannel()
 
-        btnSaveMessage.setOnClickListener {
-            val alarmManager =
-                this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-            intent = Intent(this, AlarmReceiver::class.java).let { intent ->
-                intent.putExtra("channelId", CHANNEL_ID)
-                intent.putExtra("title", "id")
-                PendingIntent.getBroadcast(this, System.currentTimeMillis().toInt(), intent, FLAG_UPDATE_CURRENT)
-            }//동작 확인 필요
-
-            var calendar = Calendar.getInstance()
-            calendar.timeInMillis = System.currentTimeMillis()
-            calendar.add(Calendar.SECOND, 10)
-            alarmManager?.set(
-                AlarmManager.RTC_WAKEUP,
-                SystemClock.elapsedRealtime() + 1 * 1000,
-                intent)
+        btnPickDate.setOnClickListener {
+            dialog.show()
         }
+        var h = 0
+        var m = 0
+        timePicker.setOnTimeChangedListener { _, hour, minute ->
+            alarmCalendar.set(Calendar.HOUR, hour)
+            alarmCalendar.set(Calendar.MINUTE, minute)
+        }
+
+        btnSaveMessage.setOnClickListener {
+            Log.d("yyjLog", "alarm저장: " + alarmCalendar.time)
+//            val alarmManager =
+//                this.getSystemService(Context.ALAaRM_SERVICE) as AlarmManager
+//
+//            intent = Intent(this, AlarmReceiver::class.java).let { intent ->
+//                intent.putExtra("channelId", CHANNEL_ID)
+//                intent.putExtra("title", "id")
+//                PendingIntent.getBroadcast(this, System.currentTimeMillis().toInt(), intent, FLAG_UPDATE_CURRENT)
+//            }
+//
+//            var calendar = Calendar.getInstance()
+//            calendar.timeInMillis = System.currentTimeMillis()
+//            calendar.add(Calendar.SECOND, 10)
+//            alarmManager?.set(
+//                AlarmManager.RTC_WAKEUP,
+//                SystemClock.elapsedRealtime() + 1 * 1000,
+//                intent)
+        }
+    }
+
+    private fun setAlarmDate() {
+        val builder = AlertDialog.Builder(this)
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_datepicker, null)
+        var calView = view.calendarView
+        calView.minDate = System.currentTimeMillis() - 1000
+        calView.setOnDateChangeListener { calendarView, year, month, day ->
+            alarmCalendar.set(year, month, day)
+            Log.d("yyjLog", "alarmCal: " + alarmCalendar.time)
+            dialog.dismiss()
+        }
+        builder.setView(view)
+        dialog = builder.create()
     }
 
     private fun createNotificationChannel() {
