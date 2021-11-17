f84c6ad9f2ee51a5be4e0c5b1ad771b9353a2d20
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-11-17 23:17:05 +0900

    RecordActivities

--- app/src/main/java/com/yyj/frecord/AddRecordActivity.kt
+++ app/src/main/java/com/yyj/frecord/AddRecordActivity.kt
@@ -1,7 +1,9 @@
 package com.yyj.frecord
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_record.*
 
 class AddRecordActivity : AppCompatActivity() {
@@ -9,5 +11,18 @@ class AddRecordActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_record)
 
+        seekBar.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                when (p1) {
+                    0 -> seekBar.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb0)
+                    1 -> seekBar.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb1)
+                    2 -> seekBar.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb2)
+                    3 -> seekBar.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb3)
+                    4 -> seekBar.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb4)
+                }
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 }
---
+++ app/src/main/java/com/yyj/frecord/AddSimpleRecordActivity.kt
@@ -0,0 +1,28 @@
+package com.yyj.frecord
+
+import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_add_simple_record.*
+
+class AddSimpleRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_simple_record)
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                when (p1) {
+                    0 -> seekBar.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb0)
+                    1 -> seekBar.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb1)
+                    2 -> seekBar.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb2)
+                    3 -> seekBar.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb3)
+                    4 -> seekBar.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb4)
+                }
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
+    }
+}
