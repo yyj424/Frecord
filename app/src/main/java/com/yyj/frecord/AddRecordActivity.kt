9ca6144c715d858fa30c9c6c710fe27c6d1c4237
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-11-19 02:03:39 +0900

    RecordActivities

--- app/src/main/java/com/yyj/frecord/AddRecordActivity.kt
+++ app/src/main/java/com/yyj/frecord/AddRecordActivity.kt
@@ -8,32 +8,31 @@ import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_record.*
 
 class AddRecordActivity : AppCompatActivity() {
-
+    private var feelingScore = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_record)
 
         val title = intent.getStringExtra("title")
-        val content = intent.getStringExtra("content")
-        var emotion = intent.getIntExtra("emotion", 2)
+        feelingScore = intent.getIntExtra("feelingScore", 2)
 
         if (title != null) {
             etRecordTitle.setText(title)
         }
-        if (content != null) {
-            etRecordContent.setText(content)
-        }
-        seekBarInFree.progress = emotion
+        setSeekBar()
 
-        tvChangeSimpleMode.setOnClickListener {
+        ivRecordSetting.setOnClickListener {
             val intent = Intent(this, AddSimpleRecordActivity::class.java)
             intent.putExtra("title", etRecordTitle.text.toString())
-            intent.putExtra("emotion", emotion)
-            intent.putExtra("content", etRecordContent.text.toString())
+            intent.putExtra("feelingScore", feelingScore)
             startActivity(intent)
             finish()
+            //dialog로 변경
         }
+    }
 
+    private fun setSeekBar() {
+        seekBarInFree.progress = feelingScore
         seekBarInFree.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 when (p1) {
@@ -43,7 +42,7 @@ class AddRecordActivity : AppCompatActivity() {
                     3 -> seekBarInFree.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb3)
                     4 -> seekBarInFree.thumb = ContextCompat.getDrawable(this@AddRecordActivity, R.drawable.ic_thumb4)
                 }
-                emotion = p1
+                feelingScore = p1
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
--- app/src/main/java/com/yyj/frecord/AddSimpleRecordActivity.kt
+++ app/src/main/java/com/yyj/frecord/AddSimpleRecordActivity.kt
@@ -2,37 +2,44 @@ package com.yyj.frecord
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_simple_record.*
 
 class AddSimpleRecordActivity : AppCompatActivity() {
+    private var feelingScore = 2
+    private var place: String? = null
+    private var feeling: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_simple_record)
 
         val title = intent.getStringExtra("title")
-        val content = intent.getStringExtra("content")
-        var emotion = intent.getIntExtra("emotion", 2)
+        feelingScore = intent.getIntExtra("feelingScore", 2)
 
         if (title != null) {
-
+            etSimpleRecordTitle.setText(title)
         }
-        if (content != null) {
 
-        }
-        seekBarInSimple.progress = emotion
+        setSeekBar()
+        setSpinners()
 
-        tvChangeFreeMode.setOnClickListener {
+        ivSimpleRecordSetting.setOnClickListener {
             val intent = Intent(this, AddRecordActivity::class.java)
-//            intent.putExtra("title", )
-            intent.putExtra("emotion", emotion)
-//            intent.putExtra("content", )
+            intent.putExtra("title", etSimpleRecordTitle.text.toString())
+            intent.putExtra("feelingScore", feelingScore)
             startActivity(intent)
             finish()
+            //dialog로 변경
         }
+    }
 
+    private fun setSeekBar() {
+        seekBarInSimple.progress = feelingScore
         seekBarInSimple.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 when (p1) {
@@ -42,10 +49,44 @@ class AddSimpleRecordActivity : AppCompatActivity() {
                     3 -> seekBarInSimple.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb3)
                     4 -> seekBarInSimple.thumb = ContextCompat.getDrawable(this@AddSimpleRecordActivity, R.drawable.ic_thumb4)
                 }
-                emotion = p1
+                feelingScore = p1
             }
             override fun onStartTrackingTouch(p0: SeekBar?) {}
             override fun onStopTrackingTouch(p0: SeekBar?) {}
         })
+    }
+
+    private fun setSpinners() {
+        placeSpinner.adapter = ArrayAdapter.createFromResource(this, R.array.placeList, android.R.layout.simple_list_item_1)
+        placeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                if (pos == 0) {
+                    etSimpleRecordWhere.setText("")
+                    etSimpleRecordWhere.visibility = View.VISIBLE
+                    place = null
+                }
+                else {
+                    etSimpleRecordWhere.visibility = View.GONE
+                    place = placeSpinner.selectedItem.toString()
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        feelingSpinner.adapter = ArrayAdapter.createFromResource(this, R.array.feelingList, android.R.layout.simple_list_item_1)
+        feelingSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                if (pos == 0) {
+                    etSimpleRecordFeeling.setText("")
+                    etSimpleRecordFeeling.visibility = View.VISIBLE
+                    feeling = null
+                }
+                else {
+                    etSimpleRecordFeeling.visibility = View.GONE
+                    feeling = feelingSpinner.selectedItem.toString()
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 }
