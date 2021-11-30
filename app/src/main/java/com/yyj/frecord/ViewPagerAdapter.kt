85e09f9492df2dce97e5c19a630eecfb1b6ae409
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-11-30 20:21:05 +0900

    fcalendar

--- app/src/main/java/com/yyj/frecord/ViewPagerAdapter.kt
+++ app/src/main/java/com/yyj/frecord/ViewPagerAdapter.kt
@@ -3,10 +3,9 @@ package com.yyj.frecord
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
 
 class ViewPagerAdapter (fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
-    private val fgList = listOf<Fragment>(MessageActivity(), RecordActivity(), GraphActivity())
+    private val fgList = listOf<Fragment>(MessageActivity(), RecordActivity(), FCalendarActivity())
 
     override fun getItemCount() :Int {
         return fgList.size
